b4bccff86d7e4231b3e1d26516b68922f88679a4
Arjon200141 <aorjon123@gmail.com>
2024-08-27 01:41:04 +0600

    User info added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -28,31 +28,81 @@ async function run() {
     const bikeCollection = client.db('bike-boulevard').collection('bikes');
     const accessoriesCollection = client.db('bike-boulevard').collection('accessories');
     const reviewsCollection = client.db('bike-boulevard').collection('reviews');
+    const userCollection = client.db('bike-boulevard').collection('users');
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      try {
+        // Check if the user already exists in the database
+        const existingUser = await userCollection.findOne({ email: user.email });
+        if (existingUser) {
+          // User already exists, return the existing user's data
+          return res.send({ message: 'User already registered', user: existingUser });
+        }
+        const result = await userCollection.insertOne(user);
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ message: 'Failed to register user' });
+      }
+    });
+    
+
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token });
+    });
+
+    // middlewares 
+    const verifyToken = (req, res, next) => {
+      const authorizationHeader = req.headers.authorization;
+      if (!authorizationHeader) {
+        return res.status(401).send({ message: 'Unauthorized access' });
+      }
+      const token = authorizationHeader.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'Unauthorized access' });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      if (user?.role !== 'admin') {
+        return res.status(403).send({ message: 'Forbidden access' });
+      }
+      next();
+    };
 
     app.get('/bikes', async (req, res) => {
-        try {
-          const result = await bikeCollection.find().toArray();
-          res.send(result);
-        } catch (error) {
-          res.status(500).send({ message: 'Internal Server Error' });
-        }
-      });
+      try {
+        const result = await bikeCollection.find().toArray();
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ message: 'Internal Server Error' });
+      }
+    });
     app.get('/accessories', async (req, res) => {
-        try {
-          const result = await accessoriesCollection.find().toArray();
-          res.send(result);
-        } catch (error) {
-          res.status(500).send({ message: 'Internal Server Error' });
-        }
-      });
+      try {
+        const result = await accessoriesCollection.find().toArray();
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ message: 'Internal Server Error' });
+      }
+    });
     app.get('/reviews', async (req, res) => {
-        try {
-          const result = await reviewsCollection.find().toArray();
-          res.send(result);
-        } catch (error) {
-          res.status(500).send({ message: 'Internal Server Error' });
-        }
-      });
+      try {
+        const result = await reviewsCollection.find().toArray();
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ message: 'Internal Server Error' });
+      }
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -65,9 +115,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Bike Boulevard is running');
-  });
-  
-  app.listen(port, () => {
-    console.log(`Bike Boulevard is running on port ${port}`);
-  });
+  res.send('Bike Boulevard is running');
+});
+
+app.listen(port, () => {
+  console.log(`Bike Boulevard is running on port ${port}`);
+});
